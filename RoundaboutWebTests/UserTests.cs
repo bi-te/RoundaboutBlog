d0f0be461a0b49c8ffe41c811c1c8d69a26972c9
true-mans-world <lexus3902015@gmail.com>
2025-06-15 16:54:16 +0300

    update tests and ci/cd workflow

--- RoundaboutWebTests/UserTests.cs
+++ RoundaboutWebTests/UserTests.cs
@@ -8,19 +8,37 @@ namespace RoundaboutWebTests;
 public class UserTests : PageTest
 {
   [Test, Order(1)]
-  public async Task Test()
+  public async Task RegisterTest()
+  {
+    await Page.GotoAsync("http://localhost:5154/Identity/Account/Login?ReturnUrl=%2F");
+    await Expect(Page.GetByText("Error. You must be logged in")).ToBeVisibleAsync();
+    await Page.GetByRole(AriaRole.Link, new() { Name = "Register", Exact = true }).ClickAsync();
+    await Page.GetByPlaceholder("username").ClickAsync();
+    await Page.GetByPlaceholder("username").FillAsync("user31");
+    await Page.GetByPlaceholder("name@example.com").ClickAsync();
+    await Page.GetByPlaceholder("name@example.com").FillAsync("user31@mail.com");
+    await Page.GetByLabel("Password", new() { Exact = true }).ClickAsync();
+    await Page.GetByLabel("Password", new() { Exact = true }).FillAsync("Test_user31");
+    await Page.GetByLabel("Confirm Password").ClickAsync();
+    await Page.GetByLabel("Confirm Password").FillAsync("Test_user31");
+    await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
+    await Expect(Page.GetByText("Your registration has been")).ToBeVisibleAsync();
+  }
+  
+  [Test, Order(2)]
+  public async Task ComplexTest()
   {
     //Login
     await Page.GotoAsync("http://localhost:5154/Identity/Account/Login?ReturnUrl=%2F");
     await Page.GetByPlaceholder("name@example.com").ClickAsync();
-    await Page.GetByPlaceholder("name@example.com").FillAsync("Voked1932@teleworm.us");
+    await Page.GetByPlaceholder("name@example.com").FillAsync("user31@mail.com");
     await Page.GetByPlaceholder("password").ClickAsync();
-    await Page.GetByPlaceholder("password").FillAsync("Test2131.");
+    await Page.GetByPlaceholder("password").FillAsync("Test_user31");
     await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
     await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Blog Posts" })).ToBeVisibleAsync();
 
     // Add post
-    await Page.Locator("li").Filter(new() { HasText = "bendy31 My Profile Dashboard" }).ClickAsync();
+    await Page.Locator("li").Filter(new() { HasText = "user31 My Profile Dashboard" }).ClickAsync();
     await Page.GetByRole(AriaRole.Link, new() { Name = "Add Post" }).ClickAsync();
     await Page.GetByLabel("Title").ClickAsync();
     await Page.GetByLabel("Title").FillAsync("My First Test Post");
@@ -57,7 +75,7 @@ public class UserTests : PageTest
     await Page.GetByRole(AriaRole.Button, new() { Name = "Delete" }).ClickAsync();
 
     // Edit profile
-    await Page.GetByRole(AriaRole.Button, new() { Name = "bendy31" }).ClickAsync();
+    await Page.GetByRole(AriaRole.Button, new() { Name = "user31" }).ClickAsync();
     await Page.GetByRole(AriaRole.Link, new() { Name = "My Profile" }).ClickAsync();
     await Page.GetByPlaceholder("Please enter your first name.").ClickAsync();
     await Page.GetByPlaceholder("Please enter your first name.").FillAsync("Darius");
@@ -67,6 +85,30 @@ public class UserTests : PageTest
 
     //Logout
     await Page.GetByRole(AriaRole.Button, new() { Name = "Logout" }).ClickAsync();
+    await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Log in", Exact = true })).ToBeVisibleAsync();
+  }
+
+
+  [Test, Order(3)]
+  public async Task DeleteUserTest()
+  {
+    //Login
+    await Page.GotoAsync("http://localhost:5154/Identity/Account/Login?ReturnUrl=%2F");
+    await Page.GetByPlaceholder("name@example.com").ClickAsync();
+    await Page.GetByPlaceholder("name@example.com").FillAsync("user31@mail.com");
+    await Page.GetByPlaceholder("password").ClickAsync();
+    await Page.GetByPlaceholder("password").FillAsync("Test_user31");
+    await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+    await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Blog Posts" })).ToBeVisibleAsync();
+    
+    //Delete user
+    await Page.GetByRole(AriaRole.Button, new() { Name = "user31" }).ClickAsync();
+    await Page.GetByRole(AriaRole.Link, new() { Name = "My Profile" }).ClickAsync();
+    await Page.GetByRole(AriaRole.Link, new() { Name = "Personal data" }).ClickAsync();
+    await Page.GetByRole(AriaRole.Button, new() { Name = "Delete" }).ClickAsync();
+    await Page.GetByPlaceholder("Please enter your password.").ClickAsync();
+    await Page.GetByPlaceholder("Please enter your password.").FillAsync("Test_user31");
+    await Page.GetByRole(AriaRole.Button, new() { Name = "Delete data and close my" }).ClickAsync();
     await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Log in", Exact = true })).ToBeVisibleAsync();
   }
 }
